fe1f5485bf6a35f5e426b9f7d8453184d176eb1f
Jonathan <jsfornerod@gmail.com>
2026-03-31 01:49:10 -0400

    Agregada funcionalidad de log en MainActivity

--- app/src/main/java/com/example/proyectopruebagit/MainActivity.kt
+++ app/src/main/java/com/example/proyectopruebagit/MainActivity.kt
@@ -11,6 +11,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        // --- AQUÍ VA TU NUEVA FUNCIONALIDAD ---
+        // Comentario: Se ha implementado la lógica de bienvenida al usuario.
+        println("Log: Nueva funcionalidad ejecutada correctamente.")
+        // --------------------------------------
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
